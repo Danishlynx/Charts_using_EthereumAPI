b338bc18cb821a41021dfc7235affe0a339aa554
Danish Ali <69537135+Danishlynx@users.noreply.github.com>
2024-02-09 14:41:05 +0530

    Update log.js

--- src/components/log.js
+++ src/components/log.js
@@ -4,7 +4,7 @@ import { Network, Alchemy } from "alchemy-sdk";
 
 const getBlockLogs = () => {
   const settings = {
-    apiKey: process.env.REACT_APP_ALCHEMY_API_KEY, // Replace with your Alchemy API Key.
+    apiKey: "demo", // Replace with your Alchemy API Key.
     network: Network.ETH_MAINNET, // Replace with your network.
   };
   const alchemy = new Alchemy(settings);
