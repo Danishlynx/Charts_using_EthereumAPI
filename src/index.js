14cb65662e7873cb1ee6116a2729855dbe63a564
Danish Ali <danishlynx@gmail.com>
2024-06-13 13:18:51 +0530

    test

--- src/index.js
+++
@@ -1,7 +0,0 @@
-// index.js
-
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App"; // Import App as the default export from App.js
-
-ReactDOM.render(<App />, document.getElementById("root"));
